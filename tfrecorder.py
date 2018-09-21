69130989e81e3a4ba6e57a54c204a443dc8bb4e1
Outsider987 <t7902195204@gmail.com>
2018-09-21 22:30:55 +0800

    tfrecorder update

--- tfrecorder.py
+++ tfrecorder.py
@@ -34,6 +34,50 @@ def get_file(file_dir):
     return image_list, label_list
         
 
+
+def int64_feature(value):
+    if not isinstance(value, list):
+        value = [value]
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+# 轉Bytes資料為 tf.train.Feature 格式
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def convert_to_TFRecord(images, labels, filename):
+    n_samples = len(labels)
+    TFWriter = tf.python_io.TFRecordWriter(filename)
+
+    print('\nTransform start...')
+    for i in np.arange(0, n_samples):
+        try:
+            image = cv2.imread(images[i], 0)
+
+            if image is None:
+                print('Error image:' + images[i])
+            else:
+                image_raw = image.tostring()
+
+            label = int(labels[i])
+            
+            # 將 tf.train.Feature 合併成 tf.train.Features
+            ftrs = tf.train.Features(
+                    feature={'Label': int64_feature(label),
+                             'image_raw': bytes_feature(image_raw)}
+                   )
+        
+            # 將 tf.train.Features 轉成 tf.train.Example
+            example = tf.train.Example(features=ftrs)
+
+            # 將 tf.train.Example 寫成 tfRecord 格式
+            TFWriter.write(example.SerializeToString())
+        except IOError as e:
+            print('Skip!\n')
+
+    TFWriter.close()
+    print('Transform done!')
+
+
 def main ():
     
     cwd = os.getcwd()
@@ -44,6 +88,7 @@ def main ():
     # 取回所有檔案路徑
     images, labels = get_file(root)
     test = images
+    convert_to_TFRecord(images, labels, cwd+"\\Train.tfrecords")
     
 
 if __name__ == '__main__':
