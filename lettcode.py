00d1dd9030593894fce5d78d0f47de2474ac931f
Outsider987 <t7902195204@gmail.com>
2018-09-24 15:15:09 +0800

    lettcode update

---
+++ lettcode.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+
+
+def main(num,target):
+    map = {}
+    for a in num:
+        v=a
+        b=0
+
+    if(map[target-v] >= 0)
+    {
+        map[v] = b
+    }
+    else
+    {
+       map[v] = b
+    }
+
+
+
+if __name__=="__main__":
+    target  = 9
+    num = [2,7,11,15]
+    main(num,target) 
