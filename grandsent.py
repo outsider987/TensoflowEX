1c08810207215341e20f0e5decb725c4cbe808de
Victor <victor.chang@ulictek.com>
2018-12-13 16:17:30 +0800

    gradsent sample

---
+++ grandsent.py
@@ -0,0 +1,18 @@
+import tensorflow as tf 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+
+REAL_PARMETER = [2,1,]
+TF_PARMETER = [[5,2],[8,5],[120,50]] [0]
+
+
+x = np.linspace(-1,1,200)
+
+y_fun = lambda a,b: a * x + b
+tf_y_fun= lambda a,b: a * x + b
+
+noise = np.random.randn(200)/10
+y = y_fun(*REAL_PARMETER) + noise
+plt.scatter(x,y)
+plt.show
