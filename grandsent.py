8e321283c42e4e302546da1fa269b58534fd187b
Victor <victor.chang@ulictek.com>
2018-12-14 23:10:47 +0800

    add matplotlib

--- grandsent.py
+++ grandsent.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-REAL_PARMETER = [2,1,]
+REAL_PARMETER = [2,1]
 TF_PARMETER = [[5,2],[8,5],[120,50]] [0]
 
 
@@ -12,6 +12,7 @@ x = np.linspace(-1,1,200)
 y_fun = lambda a,b: a * x + b
 tf_y_fun= lambda a,b: a * x + b
 
+print(REAL_PARMETER)
 noise = np.random.randn(200)/10
 y = y_fun(*REAL_PARMETER) + noise
 plt.scatter(x,y)
