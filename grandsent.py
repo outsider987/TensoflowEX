2e849a54f41537600eb563fc392e2a461eedab85
Outsider987 <t7902195204@gmail.com>
2018-12-15 16:02:14 +0800

    add pressdown to cancle program

--- grandsent.py
+++ grandsent.py
@@ -1,7 +1,6 @@
-import tensorflow as tf 
-import numpy as np 
 import matplotlib.pyplot as plt
-
+import numpy as np
+import tensorflow as tf
 
 REAL_PARMETER = [2,1]
 TF_PARMETER = [[5,2],[8,5],[120,50]] [0]
@@ -16,4 +15,5 @@ print(REAL_PARMETER)
 noise = np.random.randn(200)/10
 y = y_fun(*REAL_PARMETER) + noise
 plt.scatter(x,y)
-plt.show
+plt.show()
+plt.waitforbuttonpress()
