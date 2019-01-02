af0cabbc1f09722e0c3a431523ee9c598cc5c883
Victor <victor.chang@ulictek.com>
2019-01-02 23:51:32 +0800

    update code wtih trainning

--- train.py
+++ train.py
@@ -1,9 +1,9 @@
 import tensorflow as tf 
 import numpy as np 
-import matplotlib as plt
 import input_data
 import model
 import os
+
 
 N_CLASSES = 2
 IMG_HEIGHT = 208
@@ -17,7 +17,9 @@ learning_rate = 0.0001
 def run_training():
     
     datapath =os.getcwd()
-    datapath +="\\TensoflowEX\\face_data"
+    datapath +="\\face_data"
+    logits_path =os.getcwd()
+    logits_path +="\\logits"
     imagelist,labelist =input_data.get_file(datapath)
     image_batch,label_batch = input_data.get_batchsize(imagelist,labelist,IMG_WIDTH,IMG_HEIGHT,BATCH_SIZE,CAPACITY)
 
@@ -28,25 +30,27 @@ def run_training():
 
     summary_op = tf.summary.merge_all()
     sess = tf.Session()
-    train_writer = tf.summary.FileWriter(datapath,sess.graph)
-    saver = tf.train.start_queue_runners(sess=sess)
+    train_writer = tf.summary.FileWriter(logits_path,sess.graph)
+    saver = tf.train.Saver()
 
     sess.run(tf.global_variables_initializer())
     coord =tf.train.Coordinator()
     Threads = tf.train.start_queue_runners(sess=sess,coord=coord)
 
     try:
-        for step in np.arange(MAX_STEP)
+        for step in np.arange(MAX_STEP):
             if coord.should_stop() :
                 break
             _,tra_loss,tra_acc =sess.run([train_op,train_loss,train_acc]) 
 
             if step % 50 == 0:
                 print("Step %d, train_loss = %.2f,train accuarcy = %.2f%%" %(step,tra_loss,tra_acc))
+                summary_str = sess.run(summary_op)
+                train_writer.add_summary(summary_str,step)
 
             if step % 2000 == 0 or (step + 1) == MAX_STEP:
-                checkpoint_path = os.path.join(datapath,"model.ckpt")
-                saver.save(sess,checkpoint_path,globals_step=step)
+                checkpoint_path = os.path.join(logits_path,"model.ckpt")
+                saver.save(sess,checkpoint_path,global_step=step)
 
     except tf.errors.OutOfRangeError:
         print("Done Trainng -- epoch limit reached")
@@ -56,3 +60,59 @@ def run_training():
 
     coord.join(Threads)
     sess.close()
+
+from PIL import Image
+import matplotlib.pyplot as plt
+def get_one_image (train):
+    n=len(train)
+    ind = np.random.randint(0,n)
+    img_dir = train[ind]
+
+    image = Image.open(img_dir)
+    plt.imshow(image)
+    image = image.resize([IMG_HEIGHT,IMG_WIDTH])
+    image = np.array(image)
+    return image
+    
+
+def evelaut_one_image():
+    train_dir = os.getcwd()
+    train_dir += "\\face_data"
+    train, train_label  = input_data.get_file(train_dir)
+    image_array = get_one_image(train)
+
+    with tf.Graph().as_default():
+        BATCH_SIZE = 1
+        N_CLASSES = 2
+
+        image =tf.cast(image_array,tf.float32)
+        image =tf.reshape(image,[1,IMG_WIDTH,IMG_HEIGHT,3])
+        logit = model.inference(image,BATCH_SIZE,N_CLASSES)
+        logit = tf.nn.softmax(logit)
+
+        x = tf.placeholder(tf.float32,shape=[IMG_WIDTH,IMG_HEIGHT,3])
+
+        logs_train_dir = os.getcwd()
+        logs_train_dir +="\\logits"
+        saver = tf.train.Saver()
+
+        with tf.Session() as sess:
+            print("Reading checkpoints...")
+            ckpt = tf.train.get_checkpoint_state(logs_train_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_step = ckpt.model_checkpoint_path.split("/")[-1].split("-")[-1]
+                saver.restore(sess,ckpt.model_checkpoint_path)
+                print("Loading Sucess,golbal_step is %s" % global_step)
+            else:
+                print("no checkpoint file found")
+
+            prediction = sess.run(logit,feed_dict = {x:image_array})
+            max_index = np.argmax(prediction)
+            if max_index==0:
+                print("this is a cat with possibility %.6f" %prediction[:,0])
+            else:
+                print("this is a dog with possibility %.6f" %prediction[:,1])
+
+
+if __name__ == "__main__":
+    evelaut_one_image()
